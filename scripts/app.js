127bf33edf6570ee110aed7057cfe2bf27eb0f02
Scott Wiley <wileysb@gmail.com>
2016-03-08 11:29:48 -0600

    added pseudocode separate, started array

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,61 @@
 var clients = [
 	{
-		client: 'Going to concert',
-		tops: [{
-			img: '/shirt.jpg',
-			rank: 50
-		}],
-		bottoms: [...],
-		access: [...]
-	},
-	{...},
-	{...}
-];
+    client: 'Music Festival',
+    story: “I’m 21 and headed to a music festival in California. I am going with 15 of my BFFs so I HAVE to look insta-ready. It will be warm out and I’m super hot so I definitely want to show off some skin. Music festivals are all about a boho, free-spirited lifestyle and I have to stand out for all the right reasons. Show me what you got!”
+    tops: [
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/4876/045/251/2/w/400/4876045251_6_1_1.jpg?timestamp=1454091182782'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/6819/045/251/2/w/400/6819045251_6_1_1.jpg?timestamp=1456518992909'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/4043/055/800/2/w/400/4043055800_6_1_1.jpg?timestamp=1454685238743'}
+    ],
+    bottoms: [
+      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/14E01HCRM_large.jpg'},
+      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/14E01HCRM_large.jpg '},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/0387/024/802/2/w/400/0387024802_6_1_1.jpg?timestamp=1454692390455'}
+    ],
+    accessories: [
+      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/19K35JNAV_large.jpg'},
+      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/61H24IBLK_large.jpg'},
+      {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/24R09JBLK_large.jpg'}
+    ]
 
-var seen = [];
+  },
+  {
+    client: 'Rock Concert',
+    story: “I’m a rocker chick and i’m going to see my favorite band at the Rivinia. It is a “sling back some beers and mosh around” kind of time but I can’t look like just another band groupie. I don’t want to look over the top either! Not to sound like a stereotype but I like leather, zippers and all things black and grey. Help me look good while rocking out!”
+    tops: [
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/3427/008/800/2/w/400/3427008800_6_1_1.jpg?timestamp=1453743085878'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/3046/023/322/2/w/400/3046023322_6_1_1.jpg?timestamp=1454090981625'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/1889/041/505/2/w/400/1889041505_6_1_1.jpg?timestamp=1457107298975'}
+    ],
+    bottoms: [
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/7223/042/802/2/w/400/7223042802_6_1_1.jpg?timestamp=1452877644540'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/6840/049/400/2/w/400/6840049400_6_1_1.jpg?timestamp=1454685684230'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/3057/260/800/2/w/400/3057260800_6_1_1.jpg?timestamp=1454521193694'}
+    ],
+    accessories: [
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/1/1/p/1100/101/040/2/w/560/1100101040_1_1_1.jpg?ts=1457003360366'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/1/1/p/1746/101/040/2/w/560/1746101040_1_1_1.jpg?ts=1452795758371'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/1/1/p/1661/101/040/2/w/400/1661101040_1_1_1.jpg?timestamp=1455014086474'}
+    ]
 
-// 1. Count number of clients in array
-// 2. Pick random less than number of clients
-var client = clients[random()];
-if (seen.indexOf(client) < 0) {
-	var currentScore = 0;
-	seen.push(client);
-	// Run the game in here
-	$('.story').text(client.story);
-	client.tops.forEach(function(top) {
-		$('element').append('<img data-rank="' + top.rank + '" src="' + top.img + '" />');
-	});
-
-
-	$('top').click(function(e) {
-		e.preventDefault();
-
-		var score = $(this).data('rank');
-		currentScore += score;
-	})
-}
+  },
+  {
+    client: 'Business Meeting',
+    story: “I’m 21 and headed to a music festival in California. I am going with 15 of my BFFs so I HAVE to look insta-ready. It will be warm out and I’m super hot so I definitely want to show off some skin. Music festivals are all about a boho, free-spirited lifestyle and I have to stand out for all the right reasons. Show me what you got!”
+    tops: [
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/4876/045/251/2/w/400/4876045251_6_1_1.jpg?timestamp=1454091182782'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/6819/045/251/2/w/400/6819045251_6_1_1.jpg?timestamp=1456518992909'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/4043/055/800/2/w/400/4043055800_6_1_1.jpg?timestamp=1454685238743'}
+    ],
+    bottoms: [
+      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/14E01HCRM_large.jpg'},
+      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/14E01HCRM_large.jpg '},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/0387/024/802/2/w/400/0387024802_6_1_1.jpg?timestamp=1454692390455'}
+    ],
+    accessories: [
+      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/19K35JNAV_large.jpg'},
+      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/61H24IBLK_large.jpg'},
+      {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/24R09JBLK_large.jpg'}
+    ]
+  },
+]
