9fac097ac6568a0f8beeec7ba92c8fce9484294e
Scott Wiley <wileysb@gmail.com>
2016-03-08 11:44:58 -0600

    added first five clients to array

--- scripts/app.js
+++ scripts/app.js
@@ -41,21 +41,59 @@ var clients = [
   },
   {
     client: 'Business Meeting',
-    story: “I’m 21 and headed to a music festival in California. I am going with 15 of my BFFs so I HAVE to look insta-ready. It will be warm out and I’m super hot so I definitely want to show off some skin. Music festivals are all about a boho, free-spirited lifestyle and I have to stand out for all the right reasons. Show me what you got!”
+    story: “I just got my first job out of college and my first day is next week! They said it’s business casual….but what does that even mean??? I want to be able to appear professional but not dress like my grandma. I need to demand respect and look damn good doing it. My new boss said “just a nice top and bottom”…hope she gives better direction on the job! Can you steer me in the right direction???”,
     tops: [
-      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/4876/045/251/2/w/400/4876045251_6_1_1.jpg?timestamp=1454091182782'},
-      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/6819/045/251/2/w/400/6819045251_6_1_1.jpg?timestamp=1456518992909'},
-      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/4043/055/800/2/w/400/4043055800_6_1_1.jpg?timestamp=1454685238743'}
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/2335/683/250/2/w/400/2335683250_6_1_1.jpg?timestamp=1454692518301'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/1313/020/044/2/w/400/1313020044_6_1_1.jpg?timestamp=1454092200151'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/4786/064/660/2/w/400/4786064660_6_1_1.jpg?timestamp=1455128082962'}
     ],
     bottoms: [
-      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/14E01HCRM_large.jpg'},
-      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/14E01HCRM_large.jpg '},
-      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/0387/024/802/2/w/400/0387024802_6_1_1.jpg?timestamp=1454692390455'}
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/2234/666/679/2/w/1920/2234666679_6_1_1.jpg?timestamp=1457113615429'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/2052/679/401/2/w/560/2052679401_6_1_1.jpg?ts=1453922234400'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/0/1/p/1628/035/067/2/w/400/1628035067_6_1_1.jpg?timestamp=1457107095398'}
     ],
     accessories: [
-      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/19K35JNAV_large.jpg'},
-      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/61H24IBLK_large.jpg'},
-      {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/24R09JBLK_large.jpg'}
+      {rank: 15, img: 'http://static.zara.net/photos//2016/V/1/1/p/2434/101/040/2/w/400/2434101040_2_1_1.jpg?timestamp=1453290704726'},
+      {rank: 5, img: 'http://static.zara.net/photos//2016/V/1/1/p/2202/101/040/2/w/400/2202101040_1_1_1.jpg?timestamp=1454345358076'},
+      {rank: 0, img: 'http://static.zara.net/photos//2016/V/1/1/p/1746/101/040/2/w/560/1746101040_1_1_1.jpg?ts=1452795758371'}
     ]
   },
+  {
+    client: 'Pool Party',
+    story: “My frenemy is having a pool party to celebrate the start of summer and I want to steal the spotlight. I want to look hot both in the pool and out. Something to throw on over an adorable bathing suit is a must. Plus you can’t forget the accessories….What can I wear to make sure all the guys are looking at me, just enough to piss off the host?”,
+    tops: [
+      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/03Y01JWHT_large.jpg'},
+      {rank: 5, img: 'http://mediaus.topshop.com/wcsstore/TopShopUS/images/catalog/44X05IBLK_2_thumb.jpg'},
+      {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/04W10HWHT_large.jpg'}
+    ],
+    bottoms: [
+      {rank: 15, img: 'http://mediaus.topshop.com/wcsstore/TopShopUS/images/catalog/BUNDLE_03T05JRED03B03JRED_thumb.jpg'},
+      {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/BUNDLE_03L02JBLK03L03JBLK_large.jpg'},
+      {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/03N13JNAV_large.jpg'}
+    ],
+    accessories: [
+      {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/22S14JBLK_large.jpg'},
+      {rank: 5, img: 'http://mediaus.topshop.com/wcsstore/TopShopUS/images/catalog/61E18JGLD_thumb.jpg'},
+      {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/24C13JBLK_large.jpg'}
+    ]
+  },
+  {
+    client: 'Hiking',
+    story: “I’m going hiking with my college friends and want to make sure I’m not wearing another sweaty sorority t-shirt and gym shorts. I want to be able to look cute but also be able to scale any path I want to take! It’s supposed to be cool so I want to make sure I am warm but don’t make me look dumpy!!! Any ideas?”,
+    tops:[
+      {rank: 15, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1265926-001_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=612,650&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
+      {rank: 5, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1271521-904_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
+      {rank: 0, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1284980-750_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'}
+    ],
+    bottoms: [
+      {rank: 15, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1271786-002_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
+      {rank: 5, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1271544-002_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
+      {rank: 0, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1239244-035_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'}
+    ],
+    accessories: [
+      {rank: 15, img: 'https://underarmour.scene7.com/is/image/Underarmour/1246734-467_DEFAULT?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
+      {rank: 5, img: 'https://underarmour.scene7.com/is/image/Underarmour/1257745-407_DEFAULT?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
+      {rank: 0, img: 'https://underarmour.scene7.com/is/image/Underarmour/1266422-001_DEFAULT?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'}
+    ]
+  }
 ]
