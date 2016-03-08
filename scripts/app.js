08898b9fca00441e88800ba87910a6c843d68bb1
Scott Wiley <wileysb@gmail.com>
2016-03-08 12:55:18 -0600

    add practice notes area and got loop through array to work

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 var clients = [
 	{
     client: 'Music Festival',
-    story: “I’m 21 and headed to a music festival in California. I am going with 15 of my BFFs so I HAVE to look insta-ready. It will be warm out and I’m super hot so I definitely want to show off some skin. Music festivals are all about a boho, free-spirited lifestyle and I have to stand out for all the right reasons. Show me what you got!”
+    story: "Im 21 and headed to a music festival in California. I am going with 15 of my BFFs so I HAVE to look insta-ready. It will be warm out and Im super hot so I definitely want to show off some skin. Music festivals are all about a boho, free-spirited lifestyle and I have to stand out for all the right reasons. Show me what you got!",
     tops: [
       {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/4876/045/251/2/w/400/4876045251_6_1_1.jpg?timestamp=1454091182782'},
       {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/6819/045/251/2/w/400/6819045251_6_1_1.jpg?timestamp=1456518992909'},
@@ -17,11 +17,10 @@ var clients = [
       {rank: 5, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/61H24IBLK_large.jpg'},
       {rank: 0, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/24R09JBLK_large.jpg'}
     ]
-
   },
   {
     client: 'Rock Concert',
-    story: “I’m a rocker chick and i’m going to see my favorite band at the Rivinia. It is a “sling back some beers and mosh around” kind of time but I can’t look like just another band groupie. I don’t want to look over the top either! Not to sound like a stereotype but I like leather, zippers and all things black and grey. Help me look good while rocking out!”
+    story: "I’m a rocker chick and i’m going to see my favorite band at the Rivinia. It is a “sling back some beers and mosh around” kind of time but I can’t look like just another band groupie. I don’t want to look over the top either! Not to sound like a stereotype but I like leather, zippers and all things black and grey. Help me look good while rocking out!",
     tops: [
       {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/3427/008/800/2/w/400/3427008800_6_1_1.jpg?timestamp=1453743085878'},
       {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/3046/023/322/2/w/400/3046023322_6_1_1.jpg?timestamp=1454090981625'},
@@ -37,11 +36,10 @@ var clients = [
       {rank: 5, img: 'http://static.zara.net/photos//2016/V/1/1/p/1746/101/040/2/w/560/1746101040_1_1_1.jpg?ts=1452795758371'},
       {rank: 0, img: 'http://static.zara.net/photos//2016/V/1/1/p/1661/101/040/2/w/400/1661101040_1_1_1.jpg?timestamp=1455014086474'}
     ]
-
   },
   {
     client: 'Business Meeting',
-    story: “I just got my first job out of college and my first day is next week! They said it’s business casual….but what does that even mean??? I want to be able to appear professional but not dress like my grandma. I need to demand respect and look damn good doing it. My new boss said “just a nice top and bottom”…hope she gives better direction on the job! Can you steer me in the right direction???”,
+    story: "I just got my first job out of college and my first day is next week! They said it’s business casual….but what does that even mean??? I want to be able to appear professional but not dress like my grandma. I need to demand respect and look damn good doing it. My new boss said “just a nice top and bottom”…hope she gives better direction on the job! Can you steer me in the right direction???",
     tops: [
       {rank: 15, img: 'http://static.zara.net/photos//2016/V/0/1/p/2335/683/250/2/w/400/2335683250_6_1_1.jpg?timestamp=1454692518301'},
       {rank: 5, img: 'http://static.zara.net/photos//2016/V/0/1/p/1313/020/044/2/w/400/1313020044_6_1_1.jpg?timestamp=1454092200151'},
@@ -60,7 +58,7 @@ var clients = [
   },
   {
     client: 'Pool Party',
-    story: “My frenemy is having a pool party to celebrate the start of summer and I want to steal the spotlight. I want to look hot both in the pool and out. Something to throw on over an adorable bathing suit is a must. Plus you can’t forget the accessories….What can I wear to make sure all the guys are looking at me, just enough to piss off the host?”,
+    story: "My frenemy is having a pool party to celebrate the start of summer and I want to steal the spotlight. I want to look hot both in the pool and out. Something to throw on over an adorable bathing suit is a must. Plus you can’t forget the accessories….What can I wear to make sure all the guys are looking at me, just enough to piss off the host?",
     tops: [
       {rank: 15, img: 'http://media.topshop.com/wcsstore/TopShopUS/images/catalog/03Y01JWHT_large.jpg'},
       {rank: 5, img: 'http://mediaus.topshop.com/wcsstore/TopShopUS/images/catalog/44X05IBLK_2_thumb.jpg'},
@@ -79,7 +77,7 @@ var clients = [
   },
   {
     client: 'Hiking',
-    story: “I’m going hiking with my college friends and want to make sure I’m not wearing another sweaty sorority t-shirt and gym shorts. I want to be able to look cute but also be able to scale any path I want to take! It’s supposed to be cool so I want to make sure I am warm but don’t make me look dumpy!!! Any ideas?”,
+    story: "I’m going hiking with my college friends and want to make sure I’m not wearing another sweaty sorority t-shirt and gym shorts. I want to be able to look cute but also be able to scale any path I want to take! It’s supposed to be cool so I want to make sure I am warm but don’t make me look dumpy!!! Any ideas?",
     tops:[
       {rank: 15, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1265926-001_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=612,650&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
       {rank: 5, img: 'https://underarmour.scene7.com/is/image/Underarmour/V5-1271521-904_LDF?scl=1&fmt=jpg&qlt=80&wid=612&hei=650&size=551,585&cache=on,off&bgc=f0f0f0&resMode=sharp2'},
@@ -97,3 +95,20 @@ var clients = [
     ]
   }
 ]
+
+
+var currentScore = 0;
+for (var i = 0; i<clients.length; i++){
+  $('header').append(clients[i].story);
+  var thisClient = clients[i];
+  for (var x = 0; x<clients[i].tops.length; x++){
+  $('.tops').append("<button type='button'><img src='"+thisClient.tops[x].img+"'></button>")
+  };
+  for (var y = 0; y<clients[i].bottoms.length; y++){
+  $('.bottoms').append("<button type='button'><img src='"+thisClient.bottoms[y].img+"'></button>")
+  };
+  for (var z = 0; z<clients[i].accessories.length; z++){
+  $('.accessories').append("<button type='button'><img src='"+thisClient.accessories[z].img+"'></button>")
+  };
+  alert('THIS CLIENTS GREAT!')
+};
